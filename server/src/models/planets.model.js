9111bda45a208d70a80cd6f3c570193878412311
Pongsiri Chatkaew <pongsiri.ch@inet.co.th>
2021-08-16 11:51:22 +0700

    add package.json

---
+++ server/src/models/planets.model.js
@@ -0,0 +1,61 @@
+const parse = require('csv-parse');
+const fs = require('fs');
+const path = require('path');
+
+const habitablePlanets = [];
+
+// Review of best Habitable Planets
+// https://www.centauri-dreams.org/2015/01/30/a-review-of-the-best-habitable-planet-candidates/
+
+function isHabitablePlanet(planet) {
+  return (
+    planet['koi_disposition'] === 'CONFIRMED' &&
+    planet['koi_insol'] > 0.36 &&
+    planet['koi_insol'] < 1.11 &&
+    planet['koi_prad'] < 1.6
+  );
+}
+
+/*
+const promise = new Promise((resolve,reject)=>{
+    resolve(42)
+}) 
+
+promise.then((result)=>{})
+
+const result = await promise()
+console.log(result)
+*/
+
+function loadPlanetsData() {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(    
+      path.join(__dirname, '..', '..', 'data', 'kepler_data.csv')
+    )
+      .pipe(
+        parse({
+          comment: '#',
+          columns: true,
+        })
+      )
+      .on('data', (data) => {
+        // console.log('chunk', data);
+        if (isHabitablePlanet(data)) {
+          habitablePlanets.push(data);
+        }
+      })
+      .on('error', (err) => {
+        console.log('error', err);
+        reject(err);
+      })
+      .on('end', () => {
+        console.log(`${habitablePlanets.length} habitable planets found!`);
+        resolve();
+      });
+  });
+}
+
+module.exports = {
+  loadPlanetsData,
+  planets: habitablePlanets,
+};
