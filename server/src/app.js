0cdc813b3ffbeab066ed7d988474d80cd6287bb5
Pongsiri Chatkaew <pongsiri.ch@inet.co.th>
2021-08-16 12:08:03 +0700

    add deploy script && deploy frontend in production mode

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const planetsRouter = require('./routes/planets/planets.router');
-
+const path = require('path');
 const app = express();
 
 // Access-Control-Allow-Origin: *
@@ -12,6 +12,13 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+// console.log(path.join(__dirname, '..', 'public'))
 app.use(planetsRouter);
+
+// Serve HTML
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public'));
+});
 
 module.exports = app;
